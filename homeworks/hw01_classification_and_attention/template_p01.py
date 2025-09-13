9716b7791a663c6f94660abb7a441be05afd1791
Andrey Protasov <andrey25-84@yandex.ru>
2025-09-13 14:45:58 +0500

    Update template_p01.py

--- homeworks/hw01_classification_and_attention/template_p01.py
+++ homeworks/hw01_classification_and_attention/template_p01.py
@@ -7,9 +7,9 @@ def softmax(vector):
     return: np.array of shape (n, m)
         Matrix where softmax is computed for every row independently
     '''
-    nice_vector = vector - vector.max()
+    nice_vector = vector - vector.max(axis=1, keepdims=True)
     exp_vector = np.exp(nice_vector)
-    exp_denominator = np.sum(exp_vector, axis=1)[:, np.newaxis]
+    exp_denominator = np.sum(exp_vector, axis=1, keepdims=True)[:, np.newaxis]
     softmax_ = exp_vector / exp_denominator
     return softmax_
 
@@ -22,7 +22,9 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    attention_scores = np.dot(decoder_hidden_state.T, np.dot(W_mult, encoder_hidden_states))
+    attention_weights = softmax(attention_scores)
+    attention_vector = np.dot(encoder_hidden_states, attention_weights.T)
     
     return attention_vector
 
@@ -37,6 +39,15 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+    enc_projected = np.dot(W_add_enc, encoder_hidden_states)
+    dec_projected = np.dot(W_add_dec, decoder_hidden_state)
+    combined = np.tanh(enc_projected + dec_projected)
+    attention_scores = np.dot(v_add.T, combined)
+    attention_weights = softmax(attention_scores)
+    attention_vector = np.dot(encoder_hidden_states, attention_weights.T
     
     return attention_vector
+    out_dict = {
+        'multiplicative_attention': multiplicative_attention,
+        'additive_attention': additive_attention
+    }
